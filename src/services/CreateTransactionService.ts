ced7faa8c3ea5ad89526ffad6f579b7310532f33
Alan Siqueira <alan.siqueira@outlook.com>
2020-07-17 21:49:34 -0300

    Finished challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,7 @@
-// import AppError from '../errors/AppError';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
 interface Request {
@@ -18,10 +19,36 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError(
+        'Insufficient balance to finish this transaction',
+        400,
+      );
+    }
+
+    let categoryExists = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!categoryExists) {
+      categoryExists = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryExists);
+    }
+
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
+      category: categoryExists,
     });
 
     await transactionsRepository.save(transaction);
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError("Couldn't find Transaction with that ID.", 404);
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
